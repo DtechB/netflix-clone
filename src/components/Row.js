1aca9069b202635dacb625b979ae68efbafed28a
DtechB <d.bayati28@gmail.com>
2022-04-06 16:32:39 +0430

    create trailer of each movie.

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
+import movieTrailer from "movie-trailer";
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import client from "../api/client";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLarg }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +18,25 @@ function Row({ title, fetchUrl, isLarg }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (movie) => {
+    if (trailerUrl) setTrailerUrl("");
+    else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="Row">
@@ -24,12 +45,14 @@ function Row({ title, fetchUrl, isLarg }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleMovie(movie)}
             src={base_url + (!isLarg ? movie.backdrop_path : movie.poster_path)}
             alt={movie.name}
             className={`row__poster ${isLarg && "row__posterLarg"}`}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
